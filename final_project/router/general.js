723e5403becb9c30ad2f41e870c19b2702886b87
Parthasarathy G <21729658+gpsarathy@users.noreply.github.com>
2025-02-11 20:27:15 +0530

    task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,7 +25,12 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let result = []
+  for(let book of Object.values(books)){
+    if(book.author === req.params.author)
+      result.push(book);
+  }
+  return res.status(200).json(result);
 });
 
 // Get all books based on title
